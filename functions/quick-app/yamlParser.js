d699238eb71465e4d801f2378ba5117e6488a1ce
Carlos Salinas <carlos.s@zohocorp.com>
2023-04-04 09:29:28 -0500

    refactored parser

--- functions/quick-app/yamlParser.js
+++ functions/quick-app/yamlParser.js
@@ -1,7 +1,31 @@
 const yaml = require('js-yaml');
 let fs = require('fs');
 let excel = require('exceljs');
-let { createRandomUser, createRandomAddress } = require('./fakerFuncs');
+const { faker } = require('@faker-js/faker/locale/en_US');
+
+const range = (n) => {
+  return Array.from(Array(n).keys());
+}
+
+createRandomUser = async () => {
+    const sex = faker.name.sexType();
+    const firstName = faker.name.firstName(sex);
+    const lastName = faker.name.lastName();
+    const countryCode = 'US';
+    const state = faker.address.cityByState();
+    return {
+        avatar: faker.image.avatar(),
+        birthday: faker.date.birthdate(),
+        email: faker.internet.email(firstName, lastName),
+        firstName,
+        lastName,
+        sex,
+        street: faker.address.streetAddress(),
+        city: faker.address.cityName(),
+        state: faker.address.stateAbbr(),
+        zipCode: faker.address.zipCodeByState(state),
+    }
+}
 
 exports.parseYaml = async (file) => {
   return new Promise((resolve, reject) => {
@@ -13,6 +37,10 @@ exports.parseYaml = async (file) => {
       Object.keys(contents).forEach((form) => {
         let worksheet = workbook.addWorksheet(form);
         let col = 1;
+        let count = 0;
+        let firstMock = true;
+        let mockUsersData;
+
         let headers = Object.keys(forms[form]).reduce((acc, field) => {
           if (form.toLowerCase() === '_name') {
             acc.push('first_name');
@@ -26,9 +54,42 @@ exports.parseYaml = async (file) => {
             acc.push(field.toLowerCase());
             let val = forms[form][field];
             if (val.charAt(0) === '^') {
-              let count = Number(val.slice(1, val.length - 1));
-              if (form.val)
-
+              if (firstMock) {
+                // No need to do this for subsequent fakes
+                count = Number(val.slice(1, val.length - 1));
+                firstMock = false;
+                mockUsersData = range(count).map(() => {
+                  return createRandomUser();
+                });
+              }
+              if (form.toLowerCase() === '_name') {
+                worksheet.addColumn(col, mockUsersData.map((user) => {
+                  return user.firstName;
+                }));
+                col++;
+                worksheet.addColumn(col, mockUsersData.map((user) => {
+                  return user.lastName;
+                }));
+                col++;
+              }
+              if (form.toLowerCase() === '_address') {
+                worksheet.addColumn(col, mockUsersData.map((user) => {
+                  return user.street;
+                }));
+                col++;
+                worksheet.addColumn(col, mockUsersData.map((user) => {
+                  return user.city;
+                }));
+                col++;
+                worksheet.addColumn(col, mockUsersData.map((user) => {
+                  return user.state;
+                }));
+                col++;
+                worksheet.addColumn(col, mockUsersData.map((user) => {
+                  return user.zipCode;
+                }));
+                col++;
+              }
             }
             else {
               worksheet.addColumn(col, forms[form][field].split('__'));
@@ -36,16 +97,7 @@ exports.parseYaml = async (file) => {
           }
           return acc;
         }, []);
-
-        // worksheet.columns = headers.map((header) => {
-        //   return { header: header, id: header };
-        // });
-
-        // worksheet.addRow(1, headers.reduce((acc, header) => {
-        //   acc[header] = header;
-        //   return acc;
-        // }, {})).commit();
-
+        col = 0;
       });
       resolve(workbook);
     });
