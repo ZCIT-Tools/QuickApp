bb78104c0a843ba616af43c9324d27ddfd083ab2
Carlos Salinas <carlos.s@zohocorp.com>
2023-04-05 10:56:35 -0500

    almost there

--- functions/quick-app/yamlParser.js
+++ functions/quick-app/yamlParser.js
@@ -11,58 +11,64 @@ const contains = (arr, target) => {
   return arr.reduce((acc, value) => { return acc || (target === value); }, false);
 }
 
-const createRandomUser = async () => {
+const createRandomUser = () => {
   const sex = faker.name.sexType();
   const firstName = faker.name.firstName(sex);
   const lastName = faker.name.lastName();
-  const state = faker.address.cityByState();
+  const state = faker.address.state();
   return {
     avatar: faker.image.avatar(),
     birthday: faker.date.birthdate(),
     email: faker.internet.email(firstName, lastName),
-    firstName,
-    lastName,
-    sex,
-    street: faker.address.streetAddress(),
+    first_name: firstName,
+    last_name: lastName,
+    sex: sex,
+    address_1: faker.address.streetAddress(),
     city: faker.address.cityName(),
     state: faker.address.stateAbbr(),
-    zipCode: faker.address.zipCodeByState(state),
-  }
+    zipcode: faker.address.zipCodeByState(state),
+    country: 'US'
+  };
 }
 
 exports.parseYaml = async (file) => {
   return new Promise((resolve, reject) => {
-    let stream = fs.createReadStream(file);
-    let wb = new excel.Workbook();
+    const stream = fs.createReadStream(file);
+    const wb = new excel.Workbook();
 
     stream.on("data", async (data) => {
-      let contents = await yaml.load(data);
+      const contents = await yaml.load(data);
       const forms = Object.keys(contents); // extract forms
-      let vals; // hold field vals
+      const vals = {}; // hold field vals
 
       forms.forEach((form) => {
-        let ws = wb.addWorksheet(form); // make sheet for each form
+        // lowercase forms
+        let wsName = form.toLowerCase();
+        let ws = wb.addWorksheet(wsName); // make sheet for each form
         let fields = Object.keys(contents[form]); // extract fields
+        vals[wsName] = {};
         let headers = fields.reduce((acc, field) => { // create sheet header
-          // handle composite name field
-          if (form.toLowerCase() === '_name') {
-            vals['name'] = contents[form][field];
+          // lowercase the field
+          let wsField = field.toLowerCase();
+          if (wsField === '_name') {
+            vals[wsName]['name'] = contents[form][field];
             acc.push('first_name');
             acc.push('last_name');
           }
           // handle composite address field
-          if (form.toLowerCase() === '_address') {
-            vals['address'] = contents[form][field];
+          if (field.toLowerCase() === '_address') {
+            vals[wsName]['address'] = contents[form][field];
             acc.push('address_1');
             acc.push('address_2');
+            acc.push('city');
             acc.push('state');
             acc.push('zipcode');
             acc.push('country');
           }
           else {
             // note headers are lowercased
-            vals[field.toLowerCase()] = contents[form][field];
-            acc.push(field.toLowerCase());
+            vals[wsName][wsField] = contents[form][field];
+            acc.push(wsField);
           }
           return acc;
         }, []);
@@ -70,7 +76,6 @@ exports.parseYaml = async (file) => {
         ws.columns = headers.map((h) => {
           return { header: h, key: h };
         });
-        ws.commit();
       });
 
       // data generation from field values
@@ -88,12 +93,12 @@ exports.parseYaml = async (file) => {
           if (field === 'first_name' || field === 'last_name') {
             oldField = 'name';
           }
-          if (contains(['address_1', 'address_2', 'city', 'state', 'zipcode'], field)) {
+          if (contains(['address_1', 'address_2', 'city', 'state', 'zipcode', 'country'], field)) {
             oldField = 'address';
           }
 
-          let val = vals[oldField];
-          if (val.charAt(0) === '^' && ws.getRow(2).getCell(field).value === null) {
+          let val = vals[form][oldField];
+          if (val.slice(0,1) === '^' && ws.getRow(2).getCell(field).value === null) {
             let count = Number(val.slice(1, val.length));
             let randomUsers = range(count).map(() => {
               return createRandomUser();
@@ -110,10 +115,8 @@ exports.parseYaml = async (file) => {
             });
           }
         });
-        ws.commit();
       });
 
-      wb.commit();
       resolve(wb);
     });
     stream.on("error", reject);
